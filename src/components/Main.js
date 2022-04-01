eda5c927104cd806355e29e480e7b7af0bc14109
JhoellOpeyemi <jhoellpelumi@gmail.com>
2022-04-01 15:55:52 +0100

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+
+const Main = () => {
+  return (
+    <div className="main-container">
+      <Header />
+      <main className="main">
+        <h1 className="hero-text">Hostel at your fingertips</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Main;
